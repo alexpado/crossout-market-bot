b47db158a428b6cbe3268b7f435fd6b91687346b
Alexandre Padovan <register@alexpado.fr>
2020-01-02 17:41:02 +0100

    Springboot Initial Project

---
+++ src/main/java/fr/alexpado/bots/cmb/CrossoutMarketBotApplication.java
@@ -0,0 +1,13 @@
+package fr.alexpado.bots.cmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossoutMarketBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossoutMarketBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/alexpado/bots/cmb/CrossoutMarketBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alexpado.bots.cmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossoutMarketBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

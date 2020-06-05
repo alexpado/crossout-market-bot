e5660b1d68e06ff5ff86ae059f74a2a44abe0013
alexpado <contact@alexpado.fr>
2020-06-05 17:41:36 +0200

    Enable watchers

--- src/main/java/fr/alexpado/bots/cmb/CrossoutMarketBotApplication.java
+++ src/main/java/fr/alexpado/bots/cmb/CrossoutMarketBotApplication.java
@@ -3,12 +3,13 @@ package fr.alexpado.bots.cmb;
 import fr.alexpado.bots.cmb.bot.DiscordBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.security.auth.login.LoginException;
 import java.util.Arrays;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class CrossoutMarketBotApplication {
 
     public static CrossoutConfiguration config;

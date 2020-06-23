d38f83ba864be49f498fd47f2cf91cbeb47a8e91
alexpado <contact@alexpado.fr>
2020-06-23 18:14:29 +0200

    Connect the MarketEndpoint to the new optimized endpoint

--- src/main/java/fr/alexpado/bots/cmb/api/MarketEndpoint.java
+++ src/main/java/fr/alexpado/bots/cmb/api/MarketEndpoint.java
@@ -19,21 +19,23 @@ public class MarketEndpoint {
      *         API Host (root) to use for this endpoint.
      */
     public MarketEndpoint(String host) {
+
         this.host = host;
     }
 
     public List<GraphSet> getMarketData(long start, long end, int itemID) throws Exception {
-        HttpRequest request = new HttpRequest(String.format("%s/market-all/%s?startTimestamp=%s&endTimestamp=%s", this.host, itemID, start, end));
-        JSONArray array = request.readJsonArray();
 
-        List<Integer> buyPrices = new ArrayList<>();
+        HttpRequest request = new HttpRequest(String.format("%s/marketrecent-all/%s?startTimestamp=%s&endTimestamp=%s", this.host, itemID, start, end));
+        JSONArray   array   = request.readJsonArray();
+
+        List<Integer> buyPrices  = new ArrayList<>();
         List<Integer> sellPrices = new ArrayList<>();
 
         if (array.length() < 2) {
             throw new IllegalStateException("Not enough data for a graph !");
         }
 
-        for (int i = 0; i < array.length(); i++) {
+        for (int i = 0 ; i < array.length() ; i++) {
             JSONArray market = array.getJSONArray(i);
 
             sellPrices.add(market.getInt(1));

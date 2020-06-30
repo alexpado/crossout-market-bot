39148fd55656eb1b0f119ce5a462b24c9e9e7e84
Alexandre Padovan <contact@alexpado.fr>
2020-06-30 13:36:41 +0200

    Interfaces

---
+++ src/main/java/fr/alexpado/bots/cmb/cleaning/interfaces/game/IType.java
@@ -0,0 +1,6 @@
+package fr.alexpado.bots.cmb.cleaning.interfaces.game;
+
+import fr.alexpado.bots.cmb.cleaning.interfaces.common.Identifiable;
+import fr.alexpado.bots.cmb.cleaning.interfaces.common.Nameable;
+
+public interface IType extends Identifiable<Integer>, Nameable {}

a5459e31a4220b543a71bc312fb75ec031070dc5
NeoCooler <neocooler29@gmail.com>
2021-06-11 20:30:13 +0200

    Add french

---
+++ data/translations/French.sql
@@ -0,0 +1,126 @@
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'categories.invalid', 'Catégorie non valide. Liste des catégories :');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.help.description', 'Afficher ce menu d\'aide :eyes:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.item.description', 'Recherchez un élément et affichez son profil. ');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.language.description', 'Changer la langue de ce serveur, de cette chaîne et la vôtre :)');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.pack.description', 'Recherchez un pack et affichez son profil. ');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.pausewatchers.description', 'Mettre en pause / reprendre vos observateurs.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.search.description', 'Rechercher un objet par sa rareté, sa catégorie et sa faction.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.translate.description', 'Obtenez des informations sur la façon d\'aider à traduire le bot.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.unwatch.description',
+        'Supprimez un observateur par son identifiant. Utilisez la commande `watchlist` pour obtenir un identifiant d\'observateur.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.watch.description', 'Créez un nouvel observateur sur un élément.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.watchersettings.description', 'Modifiez les paramètres de l\'un de vos observateurs.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'command.watchlist.description', 'Affichez votre liste d\'observateurs.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'commands.nohelp', 'Cette commande ne "" peux avoir une aide avancée.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'commands.notfound', 'Peut '' pas récupérer de l\'aide pour une commande "" qui n\'existe pas :)');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'factions.invalid', 'Faction invalide. Liste des factions:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'general.currency', 'Coins');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'general.error', 'Un problème est survenu!');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'general.forbidden', 'Vous n\'avez pas la permission de faire ça ''.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'general.invite', 'Cliquez ici pour inviter le bot.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'general.syntax', 'Mauvaise syntaxe. Utilisez `cm:help %s` pour obtenir plus d\'informations sur cette commande.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'help.description', 'Oh ? Vous avez besoin d\'aide avec les commandes? J\'ai compris !');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'items.list', 'Liste des objets trouvés:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'items.multiple',
+        'Plusieurs éléments trouvés pour votre requête. Veuillez utiliser la commande `cm:item` pour obtenir le nom complet de l\'élément.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'items.notfound', 'L\'article demandé n\'a pas pu être trouvé!');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'items.removed', 'Supprimé');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'items.unavailable', 'Cet article n\'est plus disponible.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'languages.list', 'Langues prises en charge:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'languages.notsupported',
+        'Cette langue n\'est pas encore prise en charge, mais vous pouvez aider: Tapez `cm:translate` pour obtenir plus d\'informations.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'languages.updated.channel', 'Votre langue ""s a été mise à jour.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'languages.updated.guild', 'Votre guilde ''s a été mise à jour.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'languages.updated.user', 'Votre langue a été mise à jour.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'market.buy', 'Achetez-le pour:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'market.crafts.buy', 'Achetez les objets  pour:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'market.crafts.sell', 'Vendez les objets pour:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'market.sell', 'Vendez-le pour:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'packs.list', 'Liste des packs trouvés:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'packs.notfound', 'Le pack demandé n\'a pas pu être trouvé!');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'packs.price', 'Prix du pack:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'rarities.invalid', 'Rareté non valide. Liste des raretés:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'types.invalid', 'Type non valide. Liste des types:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.forbidden', 'L\'observateur demandé n\'est pas le vôtre: ''(');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.list', 'Liste de vos observateurs:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.new', 'Votre observateur a été ajouté à votre liste d\'observateurs');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.none', 'Vous n\'avez pas d\'observateurs. ''');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.notfound', 'L\'observateur demandé n\'a pas pu être trouvé! ''.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.paused', 'Vos observateurs ont été mis en pause.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.removed', 'Votre observateur a été supprimé car l\'élément aussi! :thinking:');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.resumed', 'Vos observateurs ont repris.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.buy.over', 'le prix d\'achat est terminé %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.buy.under', 'le prix d\'achat est inférieur %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.normal', '%s chaque %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.other', '%s lorsque %s chaque %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.sell.over', 'le prix de vente est terminé %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.type.sell.under', 'le prix de vente est inférieur %s');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.unwatch', 'Votre observateur a été supprimé !');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.updated', 'Votre observateur a été mis à jour !');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.wrong.for',
+        'Lors de l\'utilisation `when` ou `every` mot-clé, vous devez utiliser le `for` mot-clé pour le nom de l\'article. Ex : `cm:watch lorsque le prix d\'achat est supérieur à 5,5 tous les 2h30 pour Growl`.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.wrong.id', 'Mauvais identifiant d\'observateur. Veuillez saisir un numéro valide.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.wrong.price', 'Mauvais prix. Veuillez saisir un prix valide.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.wrong.type', 'Mauvais type d\'observateur : Les valeurs valides sont `sell` et `buy`.');
+INSERT INTO `translation` (`language`, `translation_key`, `text`)
+VALUES ('fr', 'watchers.wrong.value', 'Mauvaise valeur de l\'observateur : Les valeurs valides sont `over` et `under`.');

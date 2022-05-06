e9b24a79591a44465645fb37a9d7c05ebab41a51
alexpado <contact@alexpado.fr>
2022-05-06 23:03:33 +0200

    refactor(web): Removed web dependency

--- src/main/java/xo/marketbot/configurations/interfaces/IDiscordConfiguration.java
+++ src/main/java/xo/marketbot/configurations/interfaces/IDiscordConfiguration.java
@@ -2,19 +2,7 @@ package xo.marketbot.configurations.interfaces;
 
 public interface IDiscordConfiguration {
 
-    String getClientId();
-
-    String getClientSecret();
-
-    String getGrantType();
-
-    String getRedirectUri();
-
-    String getScope();
-
     String getToken();
-
-    String getPrefix();
 
     boolean isEnabled();
 
